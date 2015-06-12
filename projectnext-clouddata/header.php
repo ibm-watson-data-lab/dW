d4795f66de39d631212a612e47597c6869ede4f3
Christian Hogan <github@infliction.org>
2015-06-12 14:38:31 -0400

    Adjusting stylesheet reference to use rawgit for proper MIME type.

--- projectnext-clouddata/header.php
+++ projectnext-clouddata/header.php
@@ -28,7 +28,7 @@ get_template_part('partials/header-after-wp-head');
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" /> 
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/css/favicon.ico" /> 
-<link rel="stylesheet" href="https://raw.githubusercontent.com/ibm-cds-labs/dW/master/css/styles.css">
+<link rel="stylesheet" href="https://rawgit.com/ibm-cds-labs/dW/master/css/styles.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
