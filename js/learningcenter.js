24eede4d5fa212ec56137764408313fa15109cf8
Raj Singh <rrsingh@us.ibm.com>
2015-07-22 20:32:01 -0700

    adding static menu

--- js/learningcenter.js
+++ js/learningcenter.js
@@ -9,9 +9,11 @@ if ( parentel ) {
 // var pathname = window.location.pathname;
 // if (pathname.startsWith('/clouddataservices/docs/dashdb/') ) {
 if ( $('#dashdb-menu') ) {
-  var h = '<div><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.kc.doc/welcome.html">Docs</a></div>';
-  h += '<div><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/tutorial/tutorials_overview.html">Tutorials</a></div>';
-  h += '<div><a href="./how-tos/#&amp;filter=[{%22key%22%3A%22technology%22%2C%22value%22%3A%22dashDB%22}]">How-Tos</a></div>';
+  var h = '<div class="btn-group" role="group">'
+  h += '<div class="btn btn-default"><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.kc.doc/welcome.html">Docs</a></div>';
+  h += '<div class="btn btn-default"><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/tutorial/tutorials_overview.html">Tutorials</a></div>';
+  h += '<div class="btn btn-default"><a href="./how-tos/#&amp;filter=[{%22key%22%3A%22technology%22%2C%22value%22%3A%22dashDB%22}]">How-Tos</a></div>';
+  h += '</div>';
 
   $('#dashdb-menu').append(h);
 }
