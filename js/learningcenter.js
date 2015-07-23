653be6625823e27cc3b8763076459bd29a7dc4de
Raj Singh <rrsingh@us.ibm.com>
2015-07-22 20:19:48 -0700

    adding static menu

--- js/learningcenter.js
+++ js/learningcenter.js
@@ -1,3 +1,17 @@
-var parentel = $('li.current_page_parent>span>a');
-var parentname = parentel.text().trim();
-$('header').prepend('<div class="learning-center-name">'+parentname+'</div>');
+// this will only exist on pages with a parent docs page
+var parentel = $('li.current_page_parent>span>a'); 
+if ( parentel ) {
+  var parentname = parentel.text().trim();
+  $('header').prepend('<div class="learning-center-name">'+parentname+'</div>');
+}
+
+// add static menu
+// var pathname = window.location.pathname;
+// if (pathname.startsWith('/clouddataservices/docs/dashdb/') ) {
+if ( $('#dashdb-menu') ) {
+  var h = '<div><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.kc.doc/welcome.html">Docs</a></div>';
+  h += '<div><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/tutorial/tutorials_overview.html">Tutorials</a></div>';
+  h += '<div><a href="./how-tos/#&amp;filter=[{%22key%22%3A%22technology%22%2C%22value%22%3A%22dashDB%22}]">How-Tos</a></div>';
+
+  $('#dashdb-menu').append(h);
+}
