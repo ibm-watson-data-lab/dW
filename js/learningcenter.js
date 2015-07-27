52336c5a94c076597416d50706ed2c8a4945bd55
Raj Singh <rrsingh@us.ibm.com>
2015-07-26 22:19:45 -0400

    learning center fixes

--- js/learningcenter.js
+++ js/learningcenter.js
@@ -8,7 +8,7 @@ else if (pathname.startsWith('/clouddataservices/docs/cloudant/') )
 if ( $('.learning-center-menu') ) {
   var h = '<ul>'
   h += '<li><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.kc.doc/welcome.html">Docs</a></li>';
-  h += '<li"><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/tutorial/tutorials_overview.html">Tutorials</a></li>';
+  h += '<li><a href="http://www-01.ibm.com/support/knowledgecenter/SS6NHC/com.ibm.swg.im.dashdb.doc/tutorial/tutorials_overview.html">Tutorials</a></li>';
   h += '<li><a href="./how-tos/#&amp;filter=[{%22key%22%3A%22technology%22%2C%22value%22%3A%22dashDB%22}]">How-Tos</a></li>';
   h += '</ul>';
 
