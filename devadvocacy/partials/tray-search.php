716fddde2ffc3f4c7f74b40cbad0d1cdce9b24a9
vabarbosa <va@us.ibm.com>
2016-10-06 02:20:37 -0400

    incorrectly referencing metrics collector instead of search service

--- devadvocacy/partials/tray-search.php
+++ devadvocacy/partials/tray-search.php
@@ -24,7 +24,7 @@
       <span class="topic-title">Technology</span>
       <ul></ul>
       <span>
-        <a href="#" class="engine-info">Powered by the Simple Metrics Collector</a>
+        <a href="#" class="engine-info">Powered by the Simple Search Service</a>
         <a href="#" class="engine-info">i</a>
       </span>
       <div class="modal powered">
@@ -33,11 +33,10 @@
           <span>What's This?</span>
           <p>
             The most popular Topics, Technologies and Languages are determined
-            by the Simple Metrics Collector - a microservice that quickly and easily
-            tracks click events of a single-page JavaScript app. See what else IBM
-            can do for you.
+            by the Simple Search Service - a microservice that lets you quickly
+            create a faceted search engine. See what else IBM can do for you.
           </p>
-          <a href="<?php echo $home?>/simple-website-metrics"><p class="powered-cta">Learn More about the Simple Metrics Collector</p></a>
+          <a href="<?php echo $home?>/simple-search-service"><p class="powered-cta">Learn More about the Simple Search Service</p></a>
         </div>
       </div>
     </div>
